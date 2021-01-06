32edf0893998af4c4484dd35c1ff8ac7ae755d33
Henri Kellock <henri.kellock@wunderdog.fi>
2021-01-06 10:58:33 +0200

    API: Cleanup unnecessary code

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
@@ -13,7 +11,5 @@ import { TodoModule } from './todo/todo.module';
     }),
     TodoModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
